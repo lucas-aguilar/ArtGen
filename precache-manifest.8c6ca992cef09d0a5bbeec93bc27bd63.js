e55a64dffb65a7a2ef9049e94c70caa4e6f762e8
lucas-aguilar <lucas.a23@gmail.com>
2020-06-08 13:51:28 -0300

    Updates

---
+++ precache-manifest.8c6ca992cef09d0a5bbeec93bc27bd63.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "614ee04d8d6973083ad0017170588b05",
+    "url": "/ArtGen/index.html"
+  },
+  {
+    "revision": "943666ba76482c6ba97a",
+    "url": "/ArtGen/static/js/2.afc87ae4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ArtGen/static/js/2.afc87ae4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f5b3711c92f33eeef73",
+    "url": "/ArtGen/static/js/main.ad06b340.chunk.js"
+  },
+  {
+    "revision": "c6f498d88f8e3b825ddb",
+    "url": "/ArtGen/static/js/runtime-main.98de142c.js"
+  },
+  {
+    "revision": "012db217df3feb0c1f599c4b1fa28f19",
+    "url": "/ArtGen/static/media/burguer.012db217.jpg"
+  },
+  {
+    "revision": "9b4b9b04114cc59f09014054d85e3709",
+    "url": "/ArtGen/static/media/mcdonalds-logo.9b4b9b04.png"
+  }
+]);
